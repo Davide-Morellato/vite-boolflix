d5320502d3110cf3a9692c711ab2cd929b60bc99
Davide-Morellato <morellato.davide@yahoo.it>
2024-04-16 15:58:59 +0200

    Change property "params" into axios.get

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,29 +1,45 @@
 <template>
   <div>
     <input type="text" placeholder="inserisci titolo da cercare" />
+    <!-- collego la funzione al button mediante il @click (v-on:click) -->
     <button @click="getResponseButton()">INVIO</button>
   </div>
 </template>
 
 <script>
+//importo axios
 import axios from "axios";
+
+//importo lo store.js
 import { store } from "../store.js";
 
 export default {
   data() {
     return {
+      //richiamo le variabili impostate nello store.js
       api_key: store.api_key,
       query: store.query,
     };
   },
 
   methods: {
+  //creo una funzione che si colleghi all'url e si prenda come parametri le proprietà presenti in store.js
     getResponseButton() {
       axios.get("https://api.themoviedb.org/3/search/movie", {
-        parameters: {
+        params: {
           api_key: this.api_key,
           query: this.query,
         },
+      }).then((res) =>{
+
+        //dichiaro una variabile in cui richiamo l'oggetto data
+        const data = res.data
+        console.log(data)
+
+        //dichiaro una variabile in cui richiamo results nei data
+        const results = data.results
+        console.log(results)
+
       });
     },
   },
--- src/store.js
+++ src/store.js
@@ -3,8 +3,8 @@ import { reactive } from 'vue'
 
 //creo due variabili in cui definisco la query e l'api_key (dal sito: themoviedb.org )
 export const store = reactive ({
-    query: 'anelli', //temporanea
-    api_key: '0b6cbc7537d3c13c1d28e60343ff3ece'
+    api_key: '0b6cbc7537d3c13c1d28e60343ff3ece',
+    query: 'harry' //temporanea
 })
 
 
