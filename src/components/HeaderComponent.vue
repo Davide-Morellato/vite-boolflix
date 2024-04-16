a7a4581e48a9bbb2626dbdd17a69f57d27dcc4f8
Davide-Morellato <morellato.davide@yahoo.it>
2024-04-16 13:51:25 +0200

    ADD: @click to button

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <input type="text" />
-    <button></button>
+    <input type="text" placeholder="inserisci titolo da cercare" />
+    <button @click="getResponseButton()">INVIO</button>
   </div>
 </template>
 
@@ -10,15 +10,21 @@ import axios from "axios";
 import { store } from "../store.js";
 
 export default {
+  data() {
+    return {
+      api_key: store.api_key,
+      query: store.query,
+    };
+  },
+
   methods: {
-    getResponse() {
-      axios.get("https://api.themoviedb.org/3/search/movie"),
-        {
-          params: {
-            query: query,
-            api_key: api_key,
-          },
-        };
+    getResponseButton() {
+      axios.get("https://api.themoviedb.org/3/search/movie", {
+        parameters: {
+          api_key: this.api_key,
+          query: this.query,
+        },
+      });
     },
   },
 };
--- src/store.js
+++ src/store.js
@@ -1,11 +1,10 @@
 
 import { reactive } from 'vue'
 
+//creo due variabili in cui definisco la query e l'api_key (dal sito: themoviedb.org )
 export const store = reactive ({
-    
+    query: 'anelli', //temporanea
+    api_key: '0b6cbc7537d3c13c1d28e60343ff3ece'
 })
 
-//creo due variabili in cui definisco la query e l'api_key (dal sito: themoviedb.org )
-let query = 'risposta';
-let api_key = '0b6cbc7537d3c13c1d28e60343ff3ece'
 
